9b9a23e46b950b4b12069c53323da83c89987e55
gitmahin <nimahin25@gmail.com>
2025-08-12 00:57:07 +0600

    updated auth ui pages

--- apps/web/src/app/auth/login/page.tsx
+++ apps/web/src/app/auth/login/page.tsx
@@ -1,17 +1,22 @@
 import { GalleryVerticalEnd } from "lucide-react"
 
 import { LoginForm } from "@/components/auth/login-form"
+import Link from "next/link"
+import Image from "next/image"
 
 export default function LoginPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
-          </div>
-          Acme Inc.
-        </a>
+        <Link href="/" className="flex items-center gap-2 self-center font-medium">
+             <Image
+                    src={"/fnx_logo.png"}
+                    width={500}
+                    height={300}
+                    className="h-[35px] object-contain w-fit"
+                    alt="app-logo"
+                  />
+        </Link>
         <LoginForm />
       </div>
     </div>
--- apps/web/src/components/auth/login-form.tsx
+++ apps/web/src/components/auth/login-form.tsx
@@ -10,6 +10,7 @@ import {
   Button,
   cn,
 } from "@fnx/ui";
+import { Button as RadixButton } from "@radix-ui/themes";
 import Link from "next/link";
 
 type LoginFromPropsType = {
@@ -42,15 +43,15 @@ export function LoginForm({
           <form>
             <div className="grid gap-6">
               <div className="flex flex-col gap-4">
-                <Button variant="outline" className="w-full">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <RadixButton variant="surface" className="!w-full" radius="large">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-[18px] h-[18px]" >
                     <path
                       d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
                       fill="currentColor"
                     />
                   </svg>
                   Login with Google
-                </Button>
+                </RadixButton>
               </div>
               <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                 <span className="bg-card text-muted-foreground relative z-10 px-2">
@@ -79,22 +80,22 @@ export function LoginForm({
                   </div>
                   <Input id="password" type="password" required />
                 </div>
-                <Button type="submit" className="w-full">
+                <RadixButton type="submit" className="!w-full" radius="large">
                   Login
-                </Button>
+                </RadixButton>
               </div>
-              <div className="text-center text-sm">
+              <div className="text-center text-sm text-zinc-400">
                 {
                     formType == "login" ? 
                     <>
                     Don&apos;t have an account?{" "}
-                    <Link href="/auth/signup" className="underline underline-offset-4">
+                    <Link href="/auth/signup" className="underline underline-offset-4 text-zinc-50">
                   Sign up
                 </Link>
                     </> : <>
             
                     Already have an account?{" "}
-                    <Link href="/auth/login" className="underline underline-offset-4">
+                    <Link href="/auth/login" className="underline underline-offset-4 text-zinc-50">
                   Login
                     </Link>
                     </>
