6bd9acc90c1677ada9506066b925bb1f3ccacb39
gitmahin <nimahin25@gmail.com>
2025-08-17 19:17:49 +0600

    hard code mongo uri temp

--- apps/web/src/lib/connDb.ts
+++ apps/web/src/lib/connDb.ts
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
 
+// import dotenv from "dotenv"
+
+// dotenv.config({
+//   path: "./../../.env" // adjust path as needed
+// });
+
+// console.log("database uri is", process.env.MONGO_URI)
+
 type ConnectionObject = {
   isConnected?: number;
 };
