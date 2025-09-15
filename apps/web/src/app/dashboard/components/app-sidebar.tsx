808f8676796d0f989444f3149bf76e2030b38ab9
gitmahin <nimahin25@gmail.com>
2025-09-15 23:07:36 +0600

    update profile page and app sidebar

--- apps/web/src/app/dashboard/components/app-sidebar.tsx
+++ apps/web/src/app/dashboard/components/app-sidebar.tsx
@@ -50,7 +50,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     navMain: [
       {
         title: "Dashboard",
-        url: "#",
+        url: "/dashboard",
         icon: IconDashboard,
       },
     ],
--- apps/web/src/app/dashboard/profile/page.tsx
+++ apps/web/src/app/dashboard/profile/page.tsx
@@ -1,53 +1,38 @@
 "use client";
 import { getCustomerDetailsById } from "@/actions/user.action";
 import { CustomerGetType } from "@/services";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 export default function ProfilePage() {
-  const [data, setData] = useState<CustomerGetType>();
-  const [loading, setLoading] = useState(false);
-  const handleGetCustomerDetails = async () => {
-    try {
-      const response = await getCustomerDetailsById();
-      const data = await response.data;
-      setData(data);
-    } catch (error) {
-      toast.error("Unautorized user");
-    }
-  };
-
-  useEffect(() => {
-    setLoading(true);
-    handleGetCustomerDetails();
-    setLoading(false);
-  }, []);
-
+  const {data} = useSession()
   return (
     <>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
+  
         <div className="p-3">
           <Image
-            src={`${data?.avatar_url}` || null}
+            src={`${data.user.image}` || null}
             width={200}
             height={200}
-            className="w-[200px] h-[200px] rounded-md overflow-hidden object-cover object-center mb-5"
+            className="w-[130px] h-[130px] rounded-md overflow-hidden object-cover object-center mb-5"
             alt="profile-image"
           />
           <p className="text-2xl font-medium mb-5">
-            <b>Name:</b> {data?.first_name} {data?.last_name}
+            <b>Name:</b> {data?.user.name}
           </p>
           <p>
-            <b>Email: </b> {data?.email}
+            <b>Email: </b> {data?.user.email}
           </p>
           <p>
-            <b>Username: </b> {data?.username}
+            <b>Username: </b> {data?.user.email}
+          </p>
+          <p>
+            <b>Customer Id: </b> {data?.user.woo_id}
           </p>
         </div>
-      )}
+ 
     </>
   );
 }
