493cd38d5cf7f230a679d05c32a4be93dadc92d0
gitmahin <nimahin25@gmail.com>
2025-09-15 11:40:22 +0600

    update

--- apps/web/src/app/dashboard/profile/page.tsx
+++ apps/web/src/app/dashboard/profile/page.tsx
@@ -2,29 +2,28 @@ import { getCustomerDetailsById } from "@/actions/user.action";
 import Image from "next/image";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 export default async function ProfilePage() {
   const response = await getCustomerDetailsById();
   const data = await response.data;
   return (
     <div className="p-3">
       <Image
-        src={`${data.avatar_url}` || null}
+        src={`${data?.avatar_url}` || null}
         width={200}
         height={200}
         className="w-[200px] h-[200px] rounded-md overflow-hidden object-cover object-center mb-5"
         alt="profile-image"
       />
       <p className="text-2xl font-medium mb-5">
-        <b>Name:</b> {data.first_name} {data.last_name}
+        <b>Name:</b> {data?.first_name} {data?.last_name}
       </p>
       <p>
-        <b>Email: </b> {data.email}
+        <b>Email: </b> {data?.email}
       </p>
       <p>
-        <b>Username: </b> {data.username}
-      </p>
-      <p>
-        <b>Username: </b> {data.username}
+        <b>Username: </b> {data?.username}
       </p>
     </div>
   );
