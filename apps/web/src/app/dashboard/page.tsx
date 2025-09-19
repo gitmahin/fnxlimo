7f1b0d69f790d8312b7814f48f6625bb1d44893c
gitmahin <nimahin25@gmail.com>
2025-09-20 02:58:47 +0600

    update

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -37,7 +37,7 @@ const GetUserReservations = gql`
   }
 `;
 
-export function formatDateTime(date: string, time: string) {
+function formatDateTime(date: string, time: string) {
   try {
     const combined = new Date(`${date}T${time}`);
     return format(combined, "PPP 'at' p");
