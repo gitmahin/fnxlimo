893ef3521cdbfc3d68d867e83548f90fde6550fb
gitmahin <nimahin25@gmail.com>
2025-09-27 21:31:57 +0600

    add base url

--- apps/web/src/lib/apolloClient.ts
+++ apps/web/src/lib/apolloClient.ts
@@ -1,7 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  // TODO: change it to real baseURI
-  uri: "https://finixlimo.com/api/graphql",
+  uri: `${process.env.BASE_URL}/api/graphql`,
   cache: new InMemoryCache(),
 });
